a077bb062cfc90fb82ebb3e83c461a66f11ee1cc
Juan Felipe Coronel <jfmcoronel@gmail.com>
2019-11-03 11:02:05 +0000

    Add missing permission removal

--- judge/jinja2/submission.py
+++ judge/jinja2/submission.py
@@ -15,7 +15,4 @@ def submission_layout(submission, profile_id, user, editable_problem_ids, comple
     if user.has_perm('judge.change_submission'):
         can_view = True
 
-    if submission.problem_id in completed_problem_ids:
-        can_view |= submission.problem.is_public or profile_id in submission.problem.tester_ids
-
     return can_view
