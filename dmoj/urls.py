1d6f178a59ac71dd0990628f25979ccaf6deda10
Juan Felipe Coronel <jfmcoronel@gmail.com>
2019-11-04 02:16:56 +0000

    Remove problematic features for locally-served instance

--- dmoj/urls.py
+++ dmoj/urls.py
@@ -62,21 +62,21 @@ register_patterns = [
     url(r'^password/change/done/$', auth_views.PasswordChangeDoneView.as_view(
         template_name='registration/password_change_done.html',
     ), name='password_change_done'),
-    url(r'^password/reset/$', auth_views.PasswordResetView.as_view(
-        template_name='registration/password_reset.html',
-        html_email_template_name='registration/password_reset_email.html',
-        email_template_name='registration/password_reset_email.txt',
-    ), name='password_reset'),
-    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
-        auth_views.PasswordResetConfirmView.as_view(
-            template_name='registration/password_reset_confirm.html',
-        ), name='password_reset_confirm'),
-    url(r'^password/reset/complete/$', auth_views.PasswordResetCompleteView.as_view(
-        template_name='registration/password_reset_complete.html',
-    ), name='password_reset_complete'),
-    url(r'^password/reset/done/$', auth_views.PasswordResetDoneView.as_view(
-        template_name='registration/password_reset_done.html',
-    ), name='password_reset_done'),
+    #url(r'^password/reset/$', auth_views.PasswordResetView.as_view(
+    #    template_name='registration/password_reset.html',
+    #    html_email_template_name='registration/password_reset_email.html',
+    #    email_template_name='registration/password_reset_email.txt',
+    #), name='password_reset'),
+    #url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+    #    auth_views.PasswordResetConfirmView.as_view(
+    #        template_name='registration/password_reset_confirm.html',
+    #    ), name='password_reset_confirm'),
+    #url(r'^password/reset/complete/$', auth_views.PasswordResetCompleteView.as_view(
+    #    template_name='registration/password_reset_complete.html',
+    #), name='password_reset_complete'),
+    #url(r'^password/reset/done/$', auth_views.PasswordResetDoneView.as_view(
+    #    template_name='registration/password_reset_done.html',
+    #), name='password_reset_done'),
     url(r'^social/error/$', register.social_auth_error, name='social_auth_error'),
 
     url(r'^2fa/$', totp.TOTPLoginView.as_view(), name='login_2fa'),
