cfb92e2ae8ec17dcbeb2a2b63f4fc8402c2e224b
OJ Sysad <sysad@oj.dcs.upd.edu.ph>
2020-09-13 22:42:32 +0000

    Finalize script for single class addition

--- @scripts/init_users_from_csv.py
+++ @scripts/init_users_from_csv.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import re
+import random
 from dataclasses import dataclass
 
 from judge.models import *
@@ -15,6 +16,13 @@ class StudentRow:
     first_name: str
     email: str
     section: str
+    display_name: str
+
+
+@dataclass
+class NewAccount:
+    student: StudentRow
+    password: str
 
 
 def parse_csv(csv):
@@ -35,13 +43,14 @@ def parse_csv(csv):
         sid = sid.replace("-", "")
 
         ret.append(StudentRow(
-            cn=cn,
-            sid=sid,
-            last_name=last_name,
-            first_name=first_name,
-            email=email,
-            section=section,
-        ))
+                cn=cn,
+                sid=sid,
+                last_name=last_name,
+                first_name=first_name,
+                email=email,
+                section=section,
+                display_name=f"{cn}_{section}_{last_name}_{first_name}",
+            ))
 
     return ret
 
@@ -57,16 +66,48 @@ def get_or_init_org(class_title, slug, handlers):
     else:
         print("Creating organization for", slug)
         org = Organization(
-            name=class_title,
-            slug=slug,
-            short_name=class_title,
-            registrant=handlers[0],
-            is_open=False,
-        )
+                name=class_title,
+                slug=slug,
+                short_name=class_title,
+                registrant=handlers[0],
+                is_open=False,
+            )
         org.save()
         org.admins.set(handlers)
 
     return org
+
+
+def _get_random(words):
+    while True:
+        word = random.choice(words).strip()
+
+        if len(word) <= 8:
+            return word
+
+
+def generate_new_password():
+    nouns = "@scripts/nouns.txt"
+    adjectives = "@scripts/adjectives.txt"
+    verbs = "@scripts/verbs.txt"
+
+    with open(nouns) as f_nouns, open(adjectives) as f_adjectives, open(verbs) as f_verbs:
+        verb = _get_random(f_verbs.readlines())
+        adj = _get_random(f_adjectives.readlines())
+        noun = _get_random(f_nouns.readlines())
+
+    return f"{verb}-{adj}-{noun}"
+
+
+def create_new_account(student):
+    new = NewAccount(
+            student=student,
+            password=generate_new_password(),
+        )
+
+    user = User.objects.create_user(student.sid, student.email, new.password)
+
+    return user, new
 
 
 def init_single_class(class_title, csv, org_slug, language_name, handlers=['jfmcoronel']):
@@ -74,22 +115,46 @@ def init_single_class(class_title, csv, org_slug, language_name, handlers=['jfmc
     lang = Language.objects.get(name=language_name)
     org = get_or_init_org(class_title, org_slug, handlers)
 
+    new_accounts = []
+    existing_accounts = []
+
     for student in parse_csv(csv):
-        display_name = f"{student.cn}_{student.section}_{student.last_name}_{student.first_name}"
-
-        filtered = User.objects.filter(username=student.sid)
-        if not filtered:
-            print("No user yet for", display_name)
+        user = User.objects.filter(username=student.sid).first()
+        if user:
+            existing_accounts.append(student)
         else:
-            profile = Profile.objects.filter(user=filtered[0]).first()
-            if not profile:
-                print("Adding profile for", display_name)
-                # new_prof, created = Profile.objects.create(user=us, language=lang, name=display_name)
+            print("Adding new user account for", student.display_name)
+            user, new = create_new_account(student)
+            new_accounts.append(new)
+            continue
 
-            print("Updating profile for", display_name)
-            profile.organizations.add(org)
-            profile.name = display_name
-            profile.save()
+        profile = Profile.objects.filter(user=user).first()
+        if not profile:
+            print("Adding profile for", student.display_name)
+            profile = Profile.objects.create(user=user, language=lang)
+
+        print("Updating profile and last name for", student.display_name)
+        profile.organizations.add(org)
+        profile.save()
+        user.last_name = student.display_name
+        user.save()
+
+    print(f"\nExisting accounts ({len(existing_accounts)})")
+    for existing in existing_accounts:
+        print(",".join([
+            existing.display_name,
+            existing.sid,
+            existing.email,
+        ]))
+
+    print(f"\nNew accounts ({len(new_accounts)})")
+    for new_account in new_accounts:
+        print(",".join([
+            new_account.student.display_name,
+            new_account.student.sid,
+            new_account.password,
+            new_account.student.email,
+        ]))
 
 
 def main():
@@ -104,24 +169,3 @@ def main():
 
 main()  # Do not use __main__
         # exec(open("@scripts/init_users_from_csv.py").read())
-
-#with open("cs12201.txt", "r") as f:
-#    data = f.readlines()
-#    for line in data:
-#        sid, display_name = line.strip().split(",")
-#        email = sid + "@oj.dcs.upd.edu.ph"
-#        filtered = User.objects.filter(username=sid)
-#        if len(filtered) == 0:
-#            print("No user yet for ", display_name)
-#        else:
-#            us = filtered[0]
-#            profile = Profile.objects.filter(user=us)
-#            if len(profile) == 0:
-#                print("Adding profile for: ", display_name)
-#                new_prof, created = Profile.objects.create(user=us, language=lang, name=display_name)
-#                new_prof.organizations.add(cs12191)
-#            else:
-#                profile[0].organizations.add(cs12191)
-#                profile[0].name = display_name
-#                profile[0].save()
-#
